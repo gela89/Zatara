95678c9e8136d36473cbf28c3b6eedb1b786080b
gela89 <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  l>
2025-06-06 23:21:41 +0400

    We added new styles to .menu-links.open and a few more CSS changes, all from VS CODE

--- src/pages/AcordeonItemComponent/AcordeonItem.jsx
+++ src/pages/AcordeonItemComponent/AcordeonItem.jsx
@@ -1,14 +1,10 @@
-import './AcordeonItem.css'
-export  const AcordeonItem =(props)=>{
-  
-  const {setValue, handClickFu, title, content,indexNum}=props;
-  return(
-  <div onClick={()=>handClickFu(indexNum)} className="Acordeon-Cont"> 
-  <h4  className={setValue === indexNum ? 'title' : null}>{title}</h4>
-   {setValue === indexNum ?
-   <p>{content}</p> : null}  
-
-
-   </div>
-    )
-}
+import "./AcordeonItem.css";
+export const AcordeonItem = (props) => {
+  const { setValue, handClickFu, title, content, indexNum } = props;
+  return (
+    <div onClick={() => handClickFu(indexNum)} className="Acordeon-Cont">
+      <h4 className={setValue === indexNum ? "title" : null}>{title}</h4>
+      {setValue === indexNum ? <p>{content}</p> : null}
+    </div>
+  );
+};
